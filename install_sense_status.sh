b0b958286e77d5e28b84a37d2ac688a001044174
Manegiste <laurent_barthelemy@protonmail.com>
2019-05-14 20:29:46 +0000

    with try /catch

--- install_sense_status.sh
+++ install_sense_status.sh
@@ -1,5 +1,5 @@
 sudo cp sense_status.service /etc/systemd/system
 sudo cp sense_status_ip.py /usr/local/bin
 sudo chmod +x /usr/local/bin/sense_status_ip.py 
-sudo pip3 install netifaces
-sudo pip3 install psutil
+#sudo pip3 install netifaces
+#sudo pip3 install psutil
